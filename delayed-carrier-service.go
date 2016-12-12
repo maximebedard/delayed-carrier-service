48c2e9b1277bd23745c8b3d30a6bbd4d62842955
Maxime Bedard <maxime.bedard@shopify.com>
2016-12-12 11:43:33 -0500

    fmt

--- delayed-carrier-service.go
+++ delayed-carrier-service.go
@@ -24,7 +24,7 @@ type Rates struct {
 }
 
 func sleep_and_return_rates(w http.ResponseWriter, r *http.Request) {
-  milliseconds := sleep()
+	milliseconds := sleep()
 
 	rates := Rates{
 		[]Rate{
@@ -48,42 +48,42 @@ func sleep_and_return_rates(w http.ResponseWriter, r *http.Request) {
 
 	json_data, err := json.Marshal(rates)
 	if err != nil {
-    fmt.Println("unable to marshal json properly" + err.Error())
-    io.WriteString(w, "{}")
+		fmt.Println("unable to marshal json properly" + err.Error())
+		io.WriteString(w, "{}")
 		return
 	}
-  logSuccess("Rendered shipping rates after ", milliseconds)
+	logSuccess("Rendered shipping rates after ", milliseconds)
 
 	w.Write(json_data)
 }
 
 func delayedAuthy(w http.ResponseWriter, r *http.Request) {
-  milliseconds := sleep()
-  logSuccess("Rendered auty/verification/* successful response after ", milliseconds)
+	milliseconds := sleep()
+	logSuccess("Rendered auty/verification/* successful response after ", milliseconds)
 
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(http.StatusOK)
-  w.Write([]byte(`{}`))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{}`))
 }
 
 func delayedAuthyPhoneIntelligence(w http.ResponseWriter, r *http.Request) {
-  milliseconds := sleep()
-  logSuccess("Rendered auty/info successful response after ", milliseconds)
+	milliseconds := sleep()
+	logSuccess("Rendered auty/info successful response after ", milliseconds)
 
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(http.StatusOK)
-  w.Write([]byte(`{"success":true, "type": "cellphone", "ported": false}`))
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"success":true, "type": "cellphone", "ported": false}`))
 }
 
 func sleep() int {
-  milliseconds := rand.Intn(7000)
+	milliseconds := rand.Intn(7000)
 	time.Sleep(time.Duration(milliseconds) * time.Millisecond)
 
-  return milliseconds
+	return milliseconds
 }
 
 func logSuccess(message string, milliseconds int) {
-  log.Println(message + strconv.Itoa(milliseconds) + " msec")
+	log.Println(message + strconv.Itoa(milliseconds) + " msec")
 }
 
 func main() {
@@ -92,8 +92,8 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 	http.HandleFunc("/", sleep_and_return_rates)
-  http.HandleFunc("/authy/verification/start", delayedAuthy)
-  http.HandleFunc("/authy/verification/check", delayedAuthy)
-  http.HandleFunc("/authy/info", delayedAuthyPhoneIntelligence)
+	http.HandleFunc("/authy/verification/start", delayedAuthy)
+	http.HandleFunc("/authy/verification/check", delayedAuthy)
+	http.HandleFunc("/authy/info", delayedAuthyPhoneIntelligence)
 	http.ListenAndServe(":"+port, nil)
 }
